b9ddb6bf72c71842b760fdda64ca980aa3c29974
wdx04 <25487439@qq.com>
2026-06-25 21:45:04 +0800

    remove RTT code from TARGET_RA

--- targets/TARGET_RENESAS/TARGET_RA/mbed_overrides.cpp
+++ targets/TARGET_RENESAS/TARGET_RA/mbed_overrides.cpp
@@ -6,29 +6,14 @@
 #include "cmsis.h"
 #include "objects.h"
 #include "platform/mbed_error.h"
-#include "RTTConsole.h"
 
 int mbed_sdk_inited = 0;
-
-#if MBED_CONF_TARGET_CONSOLE_RTT
-// Override printf to use RTT
-static RTTConsole rtt_console;
-mbed::FileHandle *mbed::mbed_override_console(int fd)
-{
-    return &rtt_console;
-}
-#endif
 
 // This function is called after RAM initialization and before main.
 extern "C" void mbed_sdk_init()
 {
     /* Enable Pin access globally */
     R_BSP_PinAccessEnable();
-
-    /* Initialize SEGGER RTT */
-#if MBED_CONF_TARGET_CONSOLE_RTT
-    SEGGER_RTT_Init();
-#endif
 
     mbed_sdk_inited = 1;
 }
