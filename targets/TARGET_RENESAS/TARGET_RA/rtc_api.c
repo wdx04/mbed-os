829d643517b28041e8bd3d45c04e79c364fee006
wdx04 <25487439@qq.com>
2026-08-06 09:47:33 +0800

    passed Greentea's RTC tests

--- targets/TARGET_RENESAS/TARGET_RA/rtc_api.c
+++ targets/TARGET_RENESAS/TARGET_RA/rtc_api.c
@@ -63,7 +63,8 @@ time_t rtc_read(void)
     t.tm_hour = rtc_time.tm_hour;
     t.tm_mday = rtc_time.tm_mday;
     t.tm_mon  = rtc_time.tm_mon;        // struct tm: 0-11
-    t.tm_year = rtc_time.tm_year;    // struct tm: since 1900
+    t.tm_year = rtc_time.tm_year - 32;    // struct tm: since 1900
+    t.tm_isdst = 0;
 
     time_t ts = 0;
     if (_rtc_maketime(&t, &ts, RTC_4_YEAR_LEAP_YEAR_SUPPORT) == false) {
@@ -90,17 +91,15 @@ void rtc_write(time_t t)
     rtc_time.tm_hour = timeinfo.tm_hour;
     rtc_time.tm_mday = timeinfo.tm_mday;
     rtc_time.tm_mon  = timeinfo.tm_mon;
-    rtc_time.tm_year = timeinfo.tm_year;
+    // ensure rtc_time.tm_year >= 100
+    rtc_time.tm_year = timeinfo.tm_year + 32;
     rtc_time.tm_wday = timeinfo.tm_wday;
+    rtc_time.tm_isdst = 0;
 
     (void) R_RTC_CalendarTimeSet(&g_rtc0_ctrl, &rtc_time);
 }
 
 void rtc_free(void)
 {
-    if (!rtc_opened) {
-        return;
-    }
-    R_RTC_Close(&g_rtc0_ctrl);
-    rtc_opened = false;
+    /* RTC counts persistently; do not close hardware or stop counter */
 }
